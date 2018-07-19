bcfb9e5297afba07e1144f498fbe8b66608860dc
nchen0 <nicky.chen0@gmail.com>
2018-07-18 22:29:19 -0500

    api called

--- weather/src/components/Main.js
+++ weather/src/components/Main.js
@@ -1,20 +1,61 @@
 import React from "react";
 import "./Main.css";
+import axios from "axios";
 
-const Main = () => {
-  return (
-    <div className="center">
-      <div className="center-text">
-        Enter a City and State
+class Main extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      city: "",
+      URL: "",
+      temperature: ""
+    };
+  }
+
+  inputCity = event => {
+    this.setState({ city: event.target.value });
+  };
+
+  submitCity = event => {
+    event.preventDefault();
+    axios
+      .get(
+        `http://api.openweathermap.org/data/2.5/weather?q=${
+          this.state.city
+        }&type=accurate&APPID=cfa73ee6bfaba6c2989e4a6bd85765de`
+      )
+      .then(response => {
+        this.setState({ temperature: response.data });
+        console.log(
+          "this.state.temperature is: ",
+          this.state.temperature
+        );
+      })
+      .catch(err => {
+        console.log("");
+      });
+  };
+
+  render() {
+    return (
+      <div className="center">
+        <div className="center-text">
+          Enter a City and State
+        </div>
+        <div>
+          <form className="main-form">
+            <input
+              onChange={this.inputCity}
+              placeholder="St. George, Utah"
+            />
+            <button onClick={this.submitCity}>
+              Get Weather
+            </button>
+          </form>
+        </div>
       </div>
-      <div>
-        <form className="main-form">
-          <input placeholder="St. George, Utah" />
-          <button>Get Weather</button>
-        </form>
-      </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default Main;
